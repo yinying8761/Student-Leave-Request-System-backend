8a87720237dd8942272f7ee2faaf8b827b3f08cd
yinying8761 <2534851657@qq.com>
2026-06-02 18:21:21 +0800

    Revert "init: 学生请销假系统后端"
    This reverts commit 775479eff2bc3869261efc4d97cb7d7d3bfa838c.

--- src/main/java/com/leave/service/ApprovalService.java
+++
@@ -1,8 +0,0 @@
-package com.leave.service;
-
-import com.leave.dto.ApprovalRequest;
-
-public interface ApprovalService {
-    void approve(ApprovalRequest request, Long approverId);
-    java.util.List<com.leave.entity.LeaveApplication> listPending(Long approverId);
-}
